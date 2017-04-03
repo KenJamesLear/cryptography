48e1fcda048f0fd3cc6455a1a70144a415ca1ae3
ken <ken@Ken>
2017-04-02 22:49:12 -0400

    Appear to have working Miller Rabin test, need to test/comment.

--- Cryptography/src/MillerRabinPrimeTest.java
+++ Cryptography/src/MillerRabinPrimeTest.java
@@ -3,7 +3,10 @@ import java.math.BigInteger;
 public class MillerRabinPrimeTest {
 	private BigInteger n;
 	private BigInteger a;
-	private BigInteger one = BigInteger.ONE;
+	private static BigInteger zero = BigInteger.ZERO;
+	private static BigInteger one = BigInteger.ONE;
+	private static BigInteger two = one.add(one);
+	private static BigInteger negativeOne = one.subtract(two);
 	
 	
 	public MillerRabinPrimeTest(String inputN, String inputA){
@@ -11,30 +14,57 @@ public class MillerRabinPrimeTest {
 		a = new BigInteger(inputA);
 	}
 	
-	public boolean run(){
+	
+	public void run(){
 		boolean result = false;
 		
 		BigInteger m = mFinder(n);
+		BigInteger b = a.modPow(m, n);
+		int bNumber = 0;
+		System.out.println("B"+ bNumber + " = " + b + " mod " + n);
+		if ((b.equals(BigInteger.ONE)) || (b.equals(negativeOne)))
+			result = true;
+		else	
+		{
+			while(true){
+				bNumber++;
+				BigInteger bNext = b.modPow(two, n);
+				System.out.println("B"+ bNumber + " = " + bNext + " mod " + n);
+				if (bNext.equals(one)){
+					break;
+				}
+				else if (bNext.equals(negativeOne)){
+					result = true;
+					break;
+				}
+				else{
+					b = bNext;
+				}
+			}
+		}
 		
-		return result;
+		if (result){
+			System.out.println(n + " is probably prime");
+		}
+		else{
+			System.out.println(n + " is probably composite");
+		}
 	}
 	
-	public static boolean isOdd(BigInteger n){
+	private static boolean isOdd(BigInteger n){
 		return !n.mod(new BigInteger("2")).equals(BigInteger.ZERO);
 	}
 	
-	public static BigInteger mFinder(BigInteger n){
+	private static BigInteger mFinder(BigInteger n){
 		//need to comment this
-		BigInteger one = BigInteger.ONE;
-		BigInteger two = one.add(one);
-		BigInteger m = BigInteger.ZERO;
-		BigInteger temp = BigInteger.ZERO;
+		BigInteger m = zero;
+		BigInteger temp = zero;
 		BigInteger nMinusOne = n.subtract(one);
 		int k = 0;
 		while(true){
 			k++;
 			temp = nMinusOne.mod(two.pow(k));
-			if (temp.equals(BigInteger.ZERO)){
+			if (temp.equals(zero)){
 				m = nMinusOne.divide(two.pow(k));
 				if (isOdd(m))
 					break;
@@ -49,58 +79,4 @@ public class MillerRabinPrimeTest {
 	}
 	
 }	
-	
-	/*public static void main(String[] args){
-		
-		//test commint two line
-		BigInteger one = BigInteger.ONE;
-		BigInteger two = one.add(one);
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		//getting the integer you want to check if it is prime
-		BigInteger n = BigInteger.ZERO;
-		String inputN = "";
-		while(true){
-			System.out.println("Odd Integer you want to check is Prime:");
-			inputN = scanner.nextLine();
-		
-			n = new BigInteger(inputN);
-			if (isOdd(n) == true){
-				break;
-			}
-			System.out.println("Invalid n, please pick an odd number");
-		}
-		
-		//finding m
-		BigInteger m = mFinder(n);
-		//System.out.println("m is odd = " + isOdd(m));
-		
-		
-		//begin step 2
-		//getting the random integer a that is between 1 and n-1
-		//might have to chance later to have multiple a's or maybe random?
-		System.out.println("Choose a random Integer such that it's greater "
-				+ "than 1 and less than (" + inputN + " minus one):" );
-		String inputA = scanner.nextLine();
-		BigInteger a = new BigInteger(inputA);
-		
-		//test modpow
-		BigInteger one = BigInteger.ONE;
-		BigInteger two = one.add(one);
-		BigInteger three = two.add(one);
-		BigInteger test = two.modPow(two, three);
-		System.out.println("test = " + test);
-		
-		//begin step 3
-		BigInteger b = a.modPow(m, n);
-		if (b.equals(BigInteger.ONE))
-			System.out.println(b + " is probably prime");
-		else
-		{
-			BigInteger bNext = b.modPow(two, n);
-		}
-		
-		scanner.close();
-	}*/
 	
--- Cryptography/src/TestMillerRabin.java
+++ Cryptography/src/TestMillerRabin.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class TestMillerRabin {
 	public static void main(String args[]){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Odd Integer you want to check is Prime:");
 		String inputN = scanner.nextLine();
-		
+		BigInteger n = new BigInteger(inputN);
+		BigInteger nMinusOne = n.subtract(BigInteger.ONE);
 		System.out.println("Choose a random Integer such that it's greater "
-				+ "than 1 and less than (" + inputN + " minus one):" );
+				+ "than 1 and less than " + nMinusOne + ":" );
 		String inputA = scanner.nextLine();
 		
 		scanner.close();
 		MillerRabinPrimeTest test = new MillerRabinPrimeTest(inputN, inputA);
-		System.out.println(test.run());
-		
+		test.run();
 		
 	}
 
