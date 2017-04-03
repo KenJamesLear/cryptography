a7d94bcd3e692f79d793d60e03d68a3cfc74bab4
KenJamesLear <kenneth.lear@scranton.edu>
2017-04-02 21:32:19 -0400

    bug fixes and more work

--- Cryptography/src/TestMillerRabin.java
+++ Cryptography/src/TestMillerRabin.java
@@ -5,13 +5,16 @@ public class TestMillerRabin {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Odd Integer you want to check is Prime:");
 		String inputN = scanner.nextLine();
-		scanner.close();
+		
 		System.out.println("Choose a random Integer such that it's greater "
 				+ "than 1 and less than (" + inputN + " minus one):" );
 		String inputA = scanner.nextLine();
 		
+		scanner.close();
 		MillerRabinPrimeTest test = new MillerRabinPrimeTest(inputN, inputA);
-		test.run();
+		System.out.println(test.run());
+		
+		
 	}
 
 	
